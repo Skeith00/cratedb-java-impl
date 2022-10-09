f708f92cd8b0b8e77a33f2ed3a1a10f623cd6170
sergi <sergi@redeye.co>
2022-10-09 21:15:12 +1000

    Adding new springboot web dependency and first empty controller

---
+++ src/main/java/com/blog/api/controller/BlogPostController.java
@@ -0,0 +1,11 @@
+package com.blog.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blog/post")
+public class BlogPostController {
+
+
+}
